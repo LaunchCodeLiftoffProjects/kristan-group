c0983b4a46b11e577aeb4c8dad1e3af3ee0a97ba
Trevor Hill <thill7006@gmail.com>
2022-01-08 16:06:30 -0600

    updated home controller
    updated home controller, added plant repository stand in, removed unused classes

--- src/main/java/org/launchcode/GardenPlanner/controllers/HomeController.java
+++ src/main/java/org/launchcode/GardenPlanner/controllers/HomeController.java
@@ -1,4 +1,38 @@
 package org.launchcode.GardenPlanner.controllers;
 
+import org.launchcode.GardenPlanner.models.Plant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+// import javax.validation.Valid;
+import java.util.Optional;
+import org.launchcode.GardenPlanner.models.PlantRepository;
+
+@Controller
+@RequestMapping("plants")
 public class HomeController {
+
+    @Autowired
+    private PlantRepository plantRepository;
+
+    @RequestMapping("")
+    public String index(Model model) {
+
+        model.addAttribute("title", "Plants");
+
+        return "index";
+    }
+
+    @GetMapping("add")
+    public String displayAddJobForm(Model model) {
+        model.addAttribute("title", "Add Plant");
+        model.addAttribute("plants", plantRepository.findAll());
+        model.addAttribute(new Plant());
+        return "add";
+    }
+
+
+
 }
