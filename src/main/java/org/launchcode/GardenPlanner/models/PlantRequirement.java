4401c1b2538895c365bf29ee7143200f05fd7a50
Henry Ell <hankster-1@hotmail.com>
2022-01-19 16:22:18 -0600

    Refactor of PlantRequirement Models

--- src/main/java/org/launchcode/GardenPlanner/models/PlantRequirement.java
+++ src/main/java/org/launchcode/GardenPlanner/models/PlantRequirement.java
@@ -17,6 +17,10 @@ public class PlantRequirement extends AbstractEntity {
 
     // @Size(max=500, message = "Description can be as long as necessary ")
     private  String description;
+    private String plantingSeason;
+    private int plantSize = 0;
+    private String sunRequirements;
+    private  String Harvesting;
 
     public PlantRequirement(){}
 
@@ -30,6 +34,39 @@ public class PlantRequirement extends AbstractEntity {
 
     public List<Plant> getPlants() {
         return plants;
+    }
+
+
+    public String getSunRequirements() {
+        return sunRequirements;
+    }
+
+    public void setSunRequirements(String sunRequirements) {
+        this.sunRequirements = sunRequirements;
+    }
+
+    public int getPlantSize() {
+        return plantSize;
+    }
+
+    public void setPlantSize(int plantSize) {
+        this.plantSize = plantSize;
+    }
+
+    public String getPlantingSeason() {
+        return plantingSeason;
+    }
+
+    public void setPlantingSeason(String plantingSeason) {
+        this.plantingSeason = plantingSeason;
+    }
+
+    public String getHarvesting() {
+        return Harvesting;
+    }
+
+    public void setHarvesting(String harvesting) {
+        Harvesting = harvesting;
     }
 }
 
