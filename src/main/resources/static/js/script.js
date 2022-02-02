2ebb30f0fa78d5f6cf3b61b175ec675bb1f90a18
Henry Ell <hankster-1@hotmail.com>
2022-02-01 19:42:53 -0600

    Api GrowStuff implemented 1

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -14,6 +14,7 @@
                         <li>Code ${json.code}</li>
                         <li>Status ${json.status}</li>
                      </ul>
+
                   `;
                });
             });
